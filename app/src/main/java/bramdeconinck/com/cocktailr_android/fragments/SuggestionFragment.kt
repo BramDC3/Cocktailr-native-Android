5c1f1a3e6f589fc191a449ec33946a821677b69a
Bram De Coninck <bram@codifly.be>
2019-04-18 23:01:35 +0200

    finish android app

--- app/src/main/java/bramdeconinck/com/cocktailr_android/fragments/SuggestionFragment.kt
+++ app/src/main/java/bramdeconinck/com/cocktailr_android/fragments/SuggestionFragment.kt
@@ -1,17 +1,23 @@
 package bramdeconinck.com.cocktailr_android.fragments
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 
 import bramdeconinck.com.cocktailr_android.R
 import kotlinx.android.synthetic.main.fragment_suggestion.*
 
 class SuggestionFragment : Fragment() {
+
+    private var madePicture: Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_suggestion, container, false)
@@ -38,7 +44,23 @@ class SuggestionFragment : Fragment() {
 
     private fun startListeners() {
         imgbtn_suggestion_camera.setOnClickListener {
+            val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 123)
+        }
 
+        btn_suggestion_add.setOnClickListener {
+            if (madePicture && !tv_suggestion_name.text.isNullOrBlank() && !tv_suggestion_description.text.isNullOrBlank()) {
+                Toast.makeText(context, "Thank you for your suggestion!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            val bitmap = data!!.extras!!.get("data") as Bitmap
+            iv_suggestion_image.setImageBitmap(bitmap)
+            madePicture = true
         }
     }
 
